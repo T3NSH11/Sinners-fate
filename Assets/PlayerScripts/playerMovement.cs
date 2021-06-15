6211b2107ccddb3fee1bf2f81d06643658f547ec
Techmarine117 <keegan.strickler@gmail.com>
2021-06-15 18:29:19 +0400

    In a test scene,added a player movement script and a script that allows the player to look around the environment, added the ability for player to crouch

---
+++ Assets/PlayerScripts/playerMovement.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = - 9.81f;
+    public Transform groundCheck;
+    
+   
+
+    public float groundDistance = 0.4f;
+
+    public LayerMask groundMask;
+    Vector3 velocity;
+    bool isGrounded;
+
+
+    private void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+
+       
+
+        
+        
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = 20f;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = 12f;
+        }
+
+
+
+        if (Input.GetKey(KeyCode.C))
+        {
+            controller.height = 0.5f;
+        }
+        else
+        {
+            controller.height = 2.0f;
+        }
+       
+       
+
+
+
+
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    
+
+
+
+
+
+}
