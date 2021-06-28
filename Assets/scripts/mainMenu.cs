8b08b9445fdc671ffcd127358137f73a7d25d220
Techmarine117 <keegan.strickler@gmail.com>
2021-06-28 14:31:00 +0400

    added a Main menu scene created a pause menu restored the player test scene

---
+++ Assets/scripts/mainMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void quitGame()
+    {
+        Debug.Log("quiting game");
+        Application.Quit();
+
+    }
+
+    public void loadlevel()
+    {
+        Debug.Log("level Loading");
+        SceneManager.LoadScene("MAP");
+    }
+}
