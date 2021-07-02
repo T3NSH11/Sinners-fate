e5c533989f46b125604afc44726636189b0d638b
Techmarine117 <keegan.strickler@gmail.com>
2021-07-02 19:35:17 +0400

    created a script to end the game

---
+++ Assets/scripts/gameEnd.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class gameEnd : MonoBehaviour
+{
+    public GameObject DeathUi;
+    public GameObject gameEndUi;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("onDeath",10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onDeath()
+    {
+        DeathUi.SetActive(true);
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+
+
+    public void onGameEnd()
+    {
+        gameEndUi.SetActive(true);
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("quiting game");
+        Application.Quit();
+
+    }
+
+    public void loadMain()
+    {
+        Debug.Log("level Loading");
+        SceneManager.LoadScene("mainMenu");
+    }
+
+    public void restartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
