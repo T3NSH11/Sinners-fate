68efacc3c64b4f2a5e1ad22356662b0da0d5a91e
Viper <viperrules4ever@gmail.com>
2021-08-14 17:28:26 +0400

    Fixed invisible bug with Drone Patrol State

--- Assets/scripts/enemy/Drone/Follow_Waypoints1.cs
+++ Assets/scripts/enemy/Drone/Follow_Waypoints1.cs
@@ -6,11 +6,11 @@ public class Follow_Waypoints1 : DroneState
 {
     public override void DroneUpdate(DroneBehavior DroneStateMachine)
     {
-        float node_Distance = Vector3.Distance(DroneStateMachine.current_SetPath.pathNodes[DroneStateMachine.currentPath_NodeID].position, transform.position);
-        transform.position = Vector3.MoveTowards(transform.position, DroneStateMachine.current_SetPath.pathNodes[DroneStateMachine.currentPath_NodeID].position, Time.deltaTime * DroneStateMachine.speed);
+        float node_Distance = Vector3.Distance(DroneStateMachine.current_SetPath.pathNodes[DroneStateMachine.currentPath_NodeID].position, DroneStateMachine.transform.position);
+        DroneStateMachine.transform.position = Vector3.MoveTowards(DroneStateMachine.transform.position, DroneStateMachine.current_SetPath.pathNodes[DroneStateMachine.currentPath_NodeID].position, Time.deltaTime * DroneStateMachine.speed);
 
-        var object_Rotation = Quaternion.LookRotation(DroneStateMachine.current_SetPath.pathNodes[DroneStateMachine.currentPath_NodeID].position - transform.position);
-        transform.rotation = Quaternion.Slerp(transform.rotation, object_Rotation, Time.deltaTime*DroneStateMachine.rotationSpeed);
+        var object_Rotation = Quaternion.LookRotation(DroneStateMachine.current_SetPath.pathNodes[DroneStateMachine.currentPath_NodeID].position - DroneStateMachine.transform.position);
+        DroneStateMachine.transform.rotation = Quaternion.Slerp(DroneStateMachine.transform.rotation, object_Rotation, Time.deltaTime*DroneStateMachine.rotationSpeed);
 
         if (node_Distance <= DroneStateMachine.waypointDist)
         {
