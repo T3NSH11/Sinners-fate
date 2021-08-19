bc7e9277a68401339508b7862a9482afa5034b35
T3NSH11 <m.zaid@student.saedubai.com>
2021-08-19 07:17:28 +0400

    changes to door lock script

--- Assets/scripts/DoorScripts/DoorLockHandler.cs
+++ Assets/scripts/DoorScripts/DoorLockHandler.cs
@@ -7,6 +7,7 @@ public class DoorLockHandler : MonoBehaviour
     public GameObject Generator;
     GameObject[] Doors;
     DoorTrigger[] DoorTrigger;
+    public Material Greenmat;
     void Start()
     {
         Doors = GameObject.FindGameObjectsWithTag("Door");
@@ -15,30 +16,39 @@ public class DoorLockHandler : MonoBehaviour
 
     void Update()
     {
-        for (int i = 0; i < Doors.Length; i++)
+        for (int i = 0; i < gameObject.transform.childCount + 1; i++)
         {
-            if (Doors[i].GetComponent<DoorTrigger>().Doorgroup == 1 && Generator.GetComponent<Generator>().NumberOfpowerCells >= 1)
+            Debug.Log(gameObject.transform.childCount);
+            if (transform.GetChild(i).GetComponent<DoorTrigger>().Doorgroup == 1 && Generator.GetComponent<Generator>().NumberOfpowerCells >= 0)
             {
-                Doors[i].GetComponent<DoorTrigger>().IsUnlocked = true;
-                Doors[i].transform.parent.GetChild(2).GetChild(0).GetComponent<Light>().color = Color.green;
+                Debug.Log("running");
+                transform.GetChild(i).GetComponent<DoorTrigger>().IsUnlocked = true;
+                transform.GetChild(i).transform.parent.GetChild(0).GetChild(2).GetChild(0).GetComponent<Light>().color = Color.green;
+                transform.GetChild(i).transform.parent.GetChild(0).GetChild(2).GetComponent<MeshRenderer>().material = Greenmat;
             }
 
-            if (Doors[i].GetComponent<DoorTrigger>().Doorgroup == 2 && Generator.GetComponent<Generator>().NumberOfpowerCells >= 2)
+            if (transform.GetChild(i).GetComponent<DoorTrigger>().Doorgroup == 2 && Generator.GetComponent<Generator>().NumberOfpowerCells >= 1)
             {
-                Doors[i].GetComponent<DoorTrigger>().IsUnlocked = true;
-                Doors[i].transform.parent.GetChild(2).GetChild(0).GetComponent<Light>().color = Color.green;
+                Debug.Log("running");
+                transform.GetChild(i).GetComponent<DoorTrigger>().IsUnlocked = true;
+                transform.GetChild(i).transform.parent.GetChild(0).GetChild(2).GetChild(0).GetComponent<Light>().color = Color.green;
+                transform.GetChild(i).transform.parent.GetChild(0).GetChild(2).GetComponent<MeshRenderer>().material = Greenmat;
             }
 
-            if (Doors[i].GetComponent<DoorTrigger>().Doorgroup == 3 && Generator.GetComponent<Generator>().NumberOfpowerCells >= 3)
+            if (transform.GetChild(i).GetComponent<DoorTrigger>().Doorgroup == 3 && Generator.GetComponent<Generator>().NumberOfpowerCells >= 2)
             {
-                Doors[i].GetComponent<DoorTrigger>().IsUnlocked = true;
-                Doors[i].transform.parent.GetChild(2).GetChild(0).GetComponent<Light>().color = Color.green;
+                Debug.Log("running");
+                transform.GetChild(i).GetComponent<DoorTrigger>().IsUnlocked = true;
+                transform.GetChild(i).transform.parent.GetChild(0).GetChild(2).GetChild(0).GetComponent<Light>().color = Color.green;
+                transform.GetChild(i).transform.parent.GetChild(0).GetChild(2).GetComponent<MeshRenderer>().material = Greenmat;
             }
 
-            if (Doors[i].GetComponent<DoorTrigger>().Doorgroup == 4 && Generator.GetComponent<Generator>().NumberOfpowerCells >= 4)
+            if (transform.GetChild(i).GetComponent<DoorTrigger>().Doorgroup == 4 && Generator.GetComponent<Generator>().NumberOfpowerCells >= 3)
             {
-                Doors[i].GetComponent<DoorTrigger>().IsUnlocked = true;
-                Doors[i].transform.parent.GetChild(2).GetChild(0).GetComponent<Light>().color = Color.green;
+                Debug.Log("running");
+                transform.GetChild(i).GetComponent<DoorTrigger>().IsUnlocked = true;
+                transform.GetChild(i).transform.parent.GetChild(0).GetChild(2).GetChild(0).GetComponent<Light>().color = Color.green;
+                transform.GetChild(i).transform.parent.GetChild(0).GetChild(2).GetComponent<MeshRenderer>().material = Greenmat;
             }
         }
     }
